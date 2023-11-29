d8e4748b1ed2224517ab1b29a778f29125e9beb8
Laguta Roman <roma_laguta@mail.ru>
2023-11-29 22:10:53 +0900

    Пример работы инкапсуляции

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        Person sarah = new Person("Sarah", 30, 101);
+//        Так он не выведет, потому что данные приватные.
+//        System.out.println(sarah.age);
+//        Так правильно, через геттер
+        System.out.println(sarah.getAge());
+//        Меняем возраст Саре
+        sarah.setAge(28);
+        System.out.println(sarah.getAge());
+        /*Этот пример наглядно показывает принцип инкапсуляции, где мы
+        * не даём напрямую доступ к переменным
+        * */
+
     }
 }

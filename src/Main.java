7867fd7c8eb9ee626b10b7dcf1209b23c4466587
Laguta Roman <roma_laguta@mail.ru>
2023-11-29 23:14:56 +0900

    Создал геймеров, следующее будет наследование

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ import javax.print.PrintService;
 public class Main {
     public static void main(String[] args) {
 
-        Person sarah = new Person("Sarah", 30, 101);
-//        Так он не выведет, потому что данные приватные.
-//        System.out.println(sarah.age);
-//        Так правильно, через геттер
-        System.out.println(sarah.getAge());
-//        Меняем возраст Саре
-        sarah.setAge(28);
-        System.out.println(sarah.getAge());
-        /*Этот пример наглядно показывает принцип инкапсуляции, где мы
-         * не даём напрямую доступ к переменным
-         * */
+//        Person sarah = new Person("Sarah", 30, 101);
+////        Так он не выведет, потому что данные приватные.
+////        System.out.println(sarah.age);
+////        Так правильно, через геттер
+//        System.out.println(sarah.getAge());
+////        Меняем возраст Саре
+//        sarah.setAge(28);
+//        System.out.println(sarah.getAge());
+//        /*Этот пример наглядно показывает принцип инкапсуляции, где мы
+//         * не даём напрямую доступ к переменным
+//         * */
 
         Person[] people = {
                 new Person("Сара", 30, 101),
@@ -33,12 +33,21 @@ public class Main {
                 new Custumers("Дмитрий", 35, 103, 12332121)
         };
 
+        Gamer[] gamers = {
+                new Gamer("Сара", 30, 101, "PlayStation"),
+                new Gamer("Иван", 31, 102,"PlayStation"),
+                new Gamer("Дмитрий", 35, 103, "PlayStation"),
+                new Gamer("Сара", 30, 101, "PlayStation"),
+                new Gamer("Иван", 31, 102, "PlayStation"),
+                new Gamer("Дмитрий", 35, 103, "PlayStation")
+        };
+
 //        Делаем перегрузку, это 2 метода которые делают одинаковые действия отличаются только аргументы которые
 //        мы передаём
 
         PrintServices printServices = new PrintServices();
         printServices.print(people);
         printServices.print(custumerss);
-
+        printServices.print(gamers);
     }
 }
--- src/PrintServices.java
+++ src/PrintServices.java
@@ -16,4 +16,14 @@ public class PrintServices {
                     + custumers.getPhoneNumber() + "; Номер карты " + custumers.getCardNumber());
         }
     }
+
+    public void print(Gamer[] gamers) {
+        System.out.println("Размер массива Геймеров: " + gamers.length);
+        for (int i = 0; i < gamers.length; i++) {
+            Gamer gamer = gamers[i];
+            System.out.println("Имя: " + gamer.getName() + "; Возраст " + gamer.getAge() + "; Телефон "
+                    + gamer.getPhoneNumber() + "; Платформа " + gamer.getPlatform());
+
+        }
+    }
 }

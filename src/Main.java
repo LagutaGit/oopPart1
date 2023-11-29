4d12e1a3144240a13447ff90ac0f41c156b77e25
Laguta Roman <roma_laguta@mail.ru>
2023-11-29 22:33:19 +0900

    Добавил людей и вывел их

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,22 @@ public class Main {
         sarah.setAge(28);
         System.out.println(sarah.getAge());
         /*Этот пример наглядно показывает принцип инкапсуляции, где мы
-        * не даём напрямую доступ к переменным
-        * */
+         * не даём напрямую доступ к переменным
+         * */
 
+        Person[] people = {
+                new Person("Сара", 30, 101),
+                new Person("Иван", 31, 102),
+                new Person("Дмитрий", 35, 103), new Person("Сара", 30, 101),
+                new Person("Иван", 31, 102),
+                new Person("Дмитрий", 35, 103)
+        };
+
+        System.out.println("Размер списка " + people.length);
+//        Распечатаем содержимое
+        for (int i = 0; i < people.length; i++) {
+            Person person = people[i];
+            System.out.println("Имя: " + person.getName() + "; Возраст: " + person.getAge() + "; Телефон: " + person.getPhoneNumber());
+        }
     }
 }
